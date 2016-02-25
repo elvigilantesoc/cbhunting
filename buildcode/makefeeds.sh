998750197b213aa8f04bd40b5cea154987d344cf
boingomw <boingo-github@mwsec.net>
2016-02-25 23:56:28 +0000

    stupid pathing issues

--- buildcode/makefeeds.sh
+++ buildcode/makefeeds.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -f
-./generate_query_feed.py -f ../raw_parts/feedinfo-Dalerts.csv -c ../raw_parts/dalerts.csv -o ../feeds/DeloitteAlerts
-./generate_query_feed.py -f ../raw_parts/feedinfo-Dwatch.csv -c ../raw_parts/dwatchlists.csv -o ../feeds/DeloitteWatchlists
-./generate_query_feed.py -f ../raw_parts/feedinfo-Dexceptions.csv -c ../raw_parts/dexceptions.csv -o ../feeds/DeloitteExceptions
+cd ../raw_parts
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dalerts.csv -c ../raw_parts/dalerts.csv -o ../feeds/DeloitteAlerts
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dwatch.csv -c ../raw_parts/dwatchlists.csv -o ../feeds/DeloitteWatchlists
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dexceptions.csv -c ../raw_parts/dexceptions.csv -o ../feeds/DeloitteExceptions
 
