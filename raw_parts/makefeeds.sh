bfd206aa7620e7beda2bb313f9a5c60519176dbd
boingomw <boingo-github@mwsec.net>
2016-02-25 21:39:42 +0000

    rearanged and made clearscript

--- raw_parts/makefeeds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -f
-./generate_query_feed.py -f feedinfo-Dalerts.csv -c dalerts.csv -o ../feeds/DeloitteAlerts
-./generate_query_feed.py -f feedinfo-Dwatch.csv -c dwatchlists.csv -o ../feeds/DeloitteWatchlists
-./generate_query_feed.py -f feedinfo-Dexceptions.csv -c dexceptions.csv -o ../feeds/DeloitteExceptions
-
