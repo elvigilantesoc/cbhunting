852f4ab5cd13b06fb7a8898f939c00a0f7eacbe4
Ian Barton <ian@ensecure.net>
2016-04-15 10:53:03 -0500

    Modified buildscript to account for pretty printing feature.

--- buildcode/makefeeds.sh
+++ buildcode/makefeeds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -f
 cd ../raw_parts
-../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dalerts.csv -c ../raw_parts/dalerts.csv -o ../feeds/DeloitteAlerts
-../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dwatch.csv -c ../raw_parts/dwatchlists.csv -o ../feeds/DeloitteWatchlists
-../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dexceptions.csv -c ../raw_parts/dexceptions.csv -o ../feeds/DeloitteExceptions
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dalerts.csv -c ../raw_parts/dalerts.csv -o ../feeds/DeloitteAlerts -p ../pretty_feed_links/DeloitteAlerts.tsv
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dwatch.csv -c ../raw_parts/dwatchlists.csv -o ../feeds/DeloitteWatchlists -p ../pretty_feed_links/DeloitteWatchlists.tsv
+../buildcode/generate_query_feed.py -f ../raw_parts/feedinfo-Dexceptions.csv -c ../raw_parts/dexceptions.csv -o ../feeds/DeloitteExceptions -p ../pretty_feed_links/DeloitteExceptions.tsv
 
